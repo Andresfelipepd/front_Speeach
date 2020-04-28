3fe66102c4ecab7de9d999f707178bae3ae5686d
humadros <63070272+humadros@users.noreply.github.com>
2020-04-28 15:15:41 -0500

    Update Dashboard.vue

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -11,7 +11,7 @@
     <v-app-bar app clipped-left color="primary" dark>
       <v-toolbar-title>
         <v-app-bar-nav-icon @click.stop="drawer = !drawer" color="white"></v-app-bar-nav-icon>
-        <span>Speach recognition tool</span>
+        <span>Sistema de Análisis Legal. Ustariz Abogados</span>
       </v-toolbar-title>
       <div class="flex-grow-1"></div>
     </v-app-bar>
